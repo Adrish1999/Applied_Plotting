6b90cbb72d67e0ec5e33541f6094bf76b725c6fe
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-10-23 10:31:57 +0530

    Create Remove_Ticks.py

---
+++ Remove_Ticks.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure()
+
+languages =['Python', 'SQL', 'Java', 'C++', 'JavaScript']
+pos = np.arange(len(languages))
+popularity = [56, 39, 34, 34, 29]
+
+plt.bar(pos, popularity, align='center')
+plt.xticks(pos, languages)
+plt.ylabel('% Popularity')
+plt.title('Top 5 Languages for Math & Data \nby % popularity on Stack Overflow', alpha=0.8)
+
+# remove all the ticks (both axes), and tick labels on the Y axis
+plt.tick_params(top='off', bottom='off', left='off', right='off', labelleft='off', labelbottom='on')
+plt.show()
